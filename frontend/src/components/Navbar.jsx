27f050b83778f74e5e323a4b2ff4dc1574672793
Namanp1207 <150473389+Namanp1207@users.noreply.github.com>
2026-06-23 01:28:08 +0530

    first commit

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -46,7 +46,7 @@ const Navbar = () => {
                 <img onClick={()=>setShowSearch(true)} src={assets.search_icon} className='w-5 cursor-pointer' alt="" />
 
                 <div className='group relative'>
-                    <img onClick={()=> token ? null : navigate('/login')} className='w-5 cursor-pointer' src={assets.profile_icon} alt="" />
+                    <img onClick={()=> token ? null : navigate('/login')} className='w-13 sm:w-5' src={assets.profile_icon} alt="" />
                     {/* Dropdown Menu */}
                     {token && 
                     <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
